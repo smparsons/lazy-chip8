fb0cd1e87afaa4a6c651c86b56af53baf1e4e945
smparsons <samuel.m.parsons@gmail.com>
2018-06-30 14:46:37 -0400

    Render a white square on the screen with a size of 64 by 32 pixels

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,24 @@
 module Main where
 
 import Graphics.Gloss
+import Data.Word
+import Data.ByteString (ByteString, pack)
+
+rgbaForWhite :: [Word8]
+rgbaForWhite = [255, 255, 255, 255]
+
+totalBytesForEmulatorGraphics :: Int
+totalBytesForEmulatorGraphics = 8192
+
+bitmapData :: ByteString 
+bitmapData = pack $ take totalBytesForEmulatorGraphics (cycle rgbaForWhite)
+
+originalPicture :: Picture
+originalPicture = bitmapOfByteString 64 32 (BitmapFormat TopToBottom PxRGBA) bitmapData True
+
+scaledPicture :: Picture
+scaledPicture = Scale 5 5 originalPicture
 
 main :: IO ()
 main = do
-  display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+  display (InWindow "Lazy-Chip8" (640, 320) (10, 10)) black scaledPicture
