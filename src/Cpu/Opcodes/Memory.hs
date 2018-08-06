53fa762068f3ea3725104db1f0137df7d1a8d306
smparsons <samuel.m.parsons@gmail.com>
2018-08-06 19:24:52 -0400

    Implemented opcode 0xFX1E

--- src/Cpu/Opcodes/Memory.hs
+++ src/Cpu/Opcodes/Memory.hs
@@ -1,6 +1,9 @@
 module Cpu.Opcodes.Memory
-( setIndexRegisterToAddress
+( setIndexRegisterToAddress,
+  addRegisterToIndexRegister
 ) where
+
+import Data.Word
 
 import Cpu.Helpers
 import Cpu.Types
@@ -17,3 +20,18 @@ setIndexRegisterToAddress chip8State =
     originalProgramCounter = programCounter chip8State
     opcode = currentOpcode chip8State 
     address = parseThreeDigitConstant opcode
+
+--0xFX1E
+addRegisterToIndexRegister :: Chip8 -> Chip8 
+addRegisterToIndexRegister chip8State =
+  chip8State {
+    indexRegister = indexRegisterValue + convertedRegisterXValue,
+    programCounter = originalProgramCounter + programCounterIncrement
+  }
+  where 
+    originalVRegisters = vRegisters chip8State 
+    originalProgramCounter = programCounter chip8State
+    opcode = currentOpcode chip8State
+    indexRegisterValue = indexRegister chip8State
+    registerXValue = getRegisterXValue opcode originalVRegisters
+    convertedRegisterXValue = fromIntegral registerXValue :: Word16
--- test/Cpu/Opcodes/MemorySpec.hs
+++ test/Cpu/Opcodes/MemorySpec.hs
@@ -7,6 +7,7 @@ import Test.Hspec
 import Cpu.Opcodes.Memory
 import Cpu.Types
 import Cpu.TestHelpers
+import qualified Data.Vector as V
 
 spec :: Spec
 spec = do
@@ -27,3 +28,23 @@ spec = do
     it "increments the program counter" $ do
       let updatedProgramCounter = programCounter resultingState 
       updatedProgramCounter `shouldBe` 0x2AE
+
+  describe "addRegisterToIndexRegister" $ do
+    let originalVRegisters = vRegisters defaultState
+
+    let initialState = defaultState {
+      currentOpcode = 0xFC1E,
+      indexRegister = 0x2BF,
+      vRegisters = V.update originalVRegisters $ V.fromList [(0xC,0x5C)],
+      programCounter = 0x2FD
+    }
+
+    let resultingState = addRegisterToIndexRegister initialState
+
+    it "adds register to index register" $ do
+      let indexRegisterValue = indexRegister resultingState
+      indexRegisterValue `shouldBe` 0x31B
+
+    it "increments the program counter" $ do
+      let updatedProgramCounter = programCounter resultingState
+      updatedProgramCounter `shouldBe` 0x2FF
