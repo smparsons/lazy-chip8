49a379f6fd88abe9d40355859dfdeeeec0017307
smparsons <samuel.m.parsons@gmail.com>
2018-08-25 16:24:53 -0400

    Added chip8 fontset. Created initial initialize function and loadFontset function.

--- src/Chip8.hs
+++ src/Chip8.hs
@@ -1,10 +1,22 @@
 module Chip8 
-( emulateCpuCycle
+( emulateCpuCycle,
+  initialize
 ) where
 
+import Data.Word
+import qualified Data.Vector as V
+
+import Constants
 import Cpu
 import Types
 
 emulateCpuCycle :: Chip8 -> Chip8
 emulateCpuCycle = decrementSoundTimer . decrementDelayTimer . executeOpcode
 
+initialize :: Chip8
+initialize = chip8InitialState { memory = loadFontset originalMemory }
+  where 
+    originalMemory = memory chip8InitialState
+
+loadFontset :: V.Vector Word8 -> V.Vector Word8
+loadFontset = undefined
