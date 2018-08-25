f7a8b351a4750f7bfedf37fc85cce77c74b0ebc2
smparsons <samuel.m.parsons@gmail.com>
2018-08-25 14:32:46 -0400

    Implemented main CPU cycle

---
+++ src/Cpu/Cycle.hs
@@ -0,0 +1,106 @@
+module Cpu.Cycle
+( emulateCpuCycle,
+  extractOpcodeFromMemory,
+  decodeOpcode,
+  parseDigitsFromOpcode,
+  decrementDelayTimer,
+  decrementSoundTimer
+) where
+
+import Data.Word
+import Data.Bits
+import qualified Data.Vector as V
+
+import Cpu.Types
+import Cpu.Opcodes.Assignment
+import Cpu.Opcodes.BitwiseOps
+import Cpu.Opcodes.Conditionals
+import Cpu.Opcodes.ConstantOps
+import Cpu.Opcodes.Display
+import Cpu.Opcodes.Flow
+import Cpu.Opcodes.KeyOps
+import Cpu.Opcodes.Math
+import Cpu.Opcodes.Memory
+import Cpu.Opcodes.Timer
+
+emulateCpuCycle :: Chip8 -> Chip8
+emulateCpuCycle = decrementSoundTimer . decrementDelayTimer . executeOpcode
+
+executeOpcode :: Chip8 -> Chip8
+executeOpcode chip8State = operation chip8State 
+  where 
+    originalProgramCounter = programCounter chip8State
+    originalMemory = memory chip8State
+    opcode = extractOpcodeFromMemory originalMemory originalProgramCounter
+    operation = decodeOpcode opcode
+
+extractOpcodeFromMemory :: V.Vector Word8 -> Word16 -> Word16
+extractOpcodeFromMemory givenMemory givenProgramCounter = 
+  leftShiftedOpcodeFirstHalf .|. opcodeSecondHalf
+  where
+    memoryIndex = fromIntegral givenProgramCounter :: Int
+    opcodeFirstHalf = (fromIntegral $ givenMemory V.! memoryIndex) :: Word16
+    opcodeSecondHalf = (fromIntegral $ givenMemory V.! (memoryIndex + 1)) :: Word16 
+    leftShiftedOpcodeFirstHalf = opcodeFirstHalf `shiftL` 8
+
+decodeOpcode :: Word16 -> (Chip8 -> Chip8)
+decodeOpcode opcode =
+  case parseDigitsFromOpcode opcode of
+    (0x0, _, 0x0) -> clearScreen
+    (0x0, _, 0xE) -> returnFromSubroutine
+    (0x1, _, _) -> jumpToAddress
+    (0x2, _, _) -> callSubroutine 
+    (0x3, _, _) -> registerEqualsConstant
+    (0x4, _, _) -> registerDoesNotEqualConstant
+    (0x5, _, _) -> registersAreEqual
+    (0x6, _, _) -> setRegisterToConstant
+    (0x7, _, _) -> addConstantToRegister
+    (0x8, _, 0x0) -> assignToRegister
+    (0x8, _, 0x1) -> bitwiseOr
+    (0x8, _, 0x2) -> bitwiseAnd 
+    (0x8, _, 0x3) -> bitwiseXor
+    (0x8, _, 0x4) -> addTwoRegisters
+    (0x8, _, 0x5) -> subtractRegister
+    (0x8, _, 0x6) -> shiftRight
+    (0x8, _, 0x7) -> subtractTwoRegisters
+    (0x8, _, 0xE) -> shiftLeft
+    (0x9, _, _) -> registersAreNotEqual
+    (0xA, _, _) -> setIndexRegisterToAddress
+    (0xB, _, _) -> jumpToAddressPlusRegisterZero
+    (0xC, _, _) -> randomBitwiseAnd
+    (0xD, _, _) -> drawGraphics
+    (0xE, _, 0xE) -> keyIsPressed
+    (0xE, _, 0x1) -> keyIsNotPressed
+    (0xF, _, 0x7) -> setRegisterToDelayTimer
+    (0xF, _, 0xA) -> awaitKeyPress
+    (0xF, 1, 0x5) -> setDelayTimerToRegister
+    (0xF, _, 0x8) -> setSoundTimerToRegister
+    (0xF, _, 0xE) -> addRegisterToIndexRegister
+    (0xF, _, 0x9) -> storeSpriteLocation
+    (0xF, _, 0x3) -> storeBCD
+    (0xF, 0x5, 0x5) -> registerDump
+    (0xF, 0x6, 0x5) -> registerLoad
+    _ -> error "Invalid Opcode"
+
+parseDigitsFromOpcode :: Word16 -> (Word16, Word16, Word16)
+parseDigitsFromOpcode opcode = (firstDigit, thirdDigit, lastDigit)
+  where 
+    firstDigit = (opcode .&. 0xF000) `shiftR` 12
+    thirdDigit = (opcode .&. 0x00F0) `shiftR` 4
+    lastDigit = opcode .&. 0x000F
+
+decrementDelayTimer :: Chip8 -> Chip8
+decrementDelayTimer chip8State = 
+  chip8State { 
+    delayTimer = if originalDelayTimer > 0 then originalDelayTimer - 1 else originalDelayTimer 
+  }
+  where 
+    originalDelayTimer = delayTimer chip8State
+
+decrementSoundTimer :: Chip8 -> Chip8
+decrementSoundTimer chip8State = 
+  chip8State { 
+    soundTimer = if originalSoundTimer > 0 then originalSoundTimer - 1 else originalSoundTimer 
+  }
+  where 
+    originalSoundTimer = soundTimer chip8State
---
+++ test/Cpu/CycleSpec.hs
@@ -0,0 +1,69 @@
+module Cpu.CycleSpec 
+( spec
+) where
+
+import Test.Hspec
+import Cpu.Cycle
+import Cpu.Types
+import Cpu.TestHelpers
+import Data.Word
+import qualified Data.Vector as V
+
+spec :: Spec
+spec = do
+  describe "extractOpcodeFromMemory" $ do
+    let emptyMemory = V.replicate 4096 0x00
+    let updatedMemory = V.update emptyMemory $ V.fromList 
+          [ (0x240,0x8A)
+          , (0x241,0x74)
+          , (0x242,0x00)
+          , (0x243,0xEE)
+          , (0x244,0x05)
+          , (0x245,0x72)
+          , (0x246,0xFA)
+          , (0x247,0x29) ]
+
+    it "correctly extracts opcodes" $ do
+      let firstOpcode = extractOpcodeFromMemory updatedMemory 0x240      
+      firstOpcode `shouldBe` 0x8A74
+      let secondOpcode = extractOpcodeFromMemory updatedMemory 0x242
+      secondOpcode `shouldBe` 0x00EE
+      let thirdOpcode = extractOpcodeFromMemory updatedMemory 0x244
+      thirdOpcode `shouldBe` 0x0572
+      let fourthOpcode = extractOpcodeFromMemory updatedMemory 0x246
+      fourthOpcode `shouldBe` 0xFA29
+
+  describe "parseDigitsFromOpcode" $ do
+    it "correctly parses first, third, and fourth digits from opcode" $ do
+      parseDigitsFromOpcode 0x85B4 `shouldBe` (0x8, 0xB, 0x4) 
+      parseDigitsFromOpcode 0x00EE `shouldBe` (0x0, 0xE, 0xE) 
+      parseDigitsFromOpcode 0x00E0 `shouldBe` (0x0, 0xE, 0x0)
+      parseDigitsFromOpcode 0xC352 `shouldBe` (0xC, 0x5, 0x2) 
+      parseDigitsFromOpcode 0xFA29 `shouldBe` (0xF, 0x2, 0x9)
+
+  describe "decrementSoundTimer" $ do
+    it "decrements sound timer" $ do
+      let initialState = defaultState { soundTimer = 0x21 }
+      let resultingState = decrementSoundTimer initialState
+      let resultingSoundTimer = soundTimer resultingState
+      resultingSoundTimer `shouldBe` 0x20
+
+    it "does not decrement sound timer when it is already at zero" $ do
+      let initialState = defaultState { soundTimer = 0x0 }
+      let resultingState = decrementSoundTimer initialState
+      let resultingSoundTimer = soundTimer resultingState
+      resultingSoundTimer `shouldBe` 0x0
+
+  describe "decrementDelayTimer" $ do
+    it "decrements delay timer" $ do
+      let initialState = defaultState { delayTimer = 0x1A }
+      let resultingState = decrementDelayTimer initialState
+      let resultingDelayTimer = delayTimer resultingState
+      resultingDelayTimer `shouldBe` 0x19
+
+    it "does not decrement delay timer when it is already at zero" $ do
+      let initialState = defaultState { delayTimer = 0x0 }
+      let resultingState = decrementDelayTimer initialState
+      let resultingDelayTimer = delayTimer resultingState
+      resultingDelayTimer `shouldBe` 0x0
+      
