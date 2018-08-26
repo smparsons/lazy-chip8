044477eb3e1a14d35545cfd2938195fa65b600b2
smparsons <samuel.m.parsons@gmail.com>
2018-08-25 20:33:16 -0400

    Some rough initial sdl code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.Environment
+import SDL
+import Linear (V4(..))
+import Control.Monad (unless)
 
 import Chip8
 import Types
@@ -17,4 +21,26 @@ startChip8Game :: String -> IO ()
 startChip8Game filepath = do
   chip8 <- initializeChip8
   chip8WithGame <- loadGameByFilePath filepath chip8
-  print chip8WithGame
+  setupChip8GameWindow
+
+setupChip8GameWindow :: IO ()
+setupChip8GameWindow = do
+  initializeAll
+  window <- createWindow "Chip-8 Emulator" defaultWindow { windowInitialSize = V2 512 256 }
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop renderer
+
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress events
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  present renderer
+  unless qPressed (appLoop renderer)
