87c7dbae87c9b80bb9777281dcc59018048a8501
smparsons <samuel.m.parsons@gmail.com>
2018-09-15 15:33:11 -0400

    Reformatting module definition in Emulator.hs

--- app/Emulator.hs
+++ app/Emulator.hs
@@ -1,10 +1,10 @@
 module Emulator 
-( module Emulator.Audio,
-  module Emulator.Events,
-  module Emulator.Graphics,
-  module Emulator.Helpers,
-  module Emulator.Types
-) where
+  ( module Emulator.Audio
+  , module Emulator.Events
+  , module Emulator.Graphics
+  , module Emulator.Helpers
+  , module Emulator.Types
+  ) where
 
 import Emulator.Audio
 import Emulator.Events
